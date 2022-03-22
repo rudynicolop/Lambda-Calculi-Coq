3d406ad232e436d6121ff4edb41c491542226e9d
rnp39 <rnp39@cornell.edu>
2022-03-21 19:37:54 -0700

    wah

--- lib/STLC/Has.v
+++ lib/STLC/Has.v
@@ -41,23 +41,23 @@ Notation "x '⋅' y"
 Equations
   Rename
   {Γ Δ} :
-  (forall {τ}, Has τ Γ -> Has τ Δ) -> forall {σ}, Γ ⊢ σ -> Δ ⊢ σ :=
+  (forall τ, Has τ Γ -> Has τ Δ) -> forall {σ}, Γ ⊢ σ -> Δ ⊢ σ :=
   Rename f (Id _ _ h)   := Id _ _ (f _ h);
   Rename f (λ ρ ⇒ t')%term := (`λ Rename (@ext_has _ _ _ f ρ) t')%term;
   Rename f (t₁ ⋅ t₂)%term  := (Rename f t₁ ⋅ Rename f t₂)%term.
 
 Equations Exts : forall {Γ Δ},
-    (forall {τ}, Has τ Γ -> Δ ⊢ τ) ->
-    forall ρ {σ}, Has σ (ρ :: Γ) -> ρ :: Δ ⊢ σ :=
-  Exts f _ HasO := Id _ _ HasO;
-  Exts f _ (HasS hs) := Rename (fun τ => @HasS _ τ _ _) (f _ hs).
+    (forall τ, Has τ Γ -> Δ ⊢ τ) ->
+    forall ρ σ, Has σ (ρ :: Γ) -> ρ :: Δ ⊢ σ :=
+  Exts f _ _ HasO := Id _ _ HasO;
+  Exts f _ _ (HasS hs) := Rename (fun τ => @HasS _ τ _ _) (f _ hs).
 
 Equations
   subs : forall {Γ Δ},
     (forall {τ}, Has τ Γ -> Δ ⊢ τ) ->
     forall {σ}, Γ ⊢ σ -> Δ ⊢ σ :=
   subs f (Id _ _ h)   := f _ h;
-  subs f (λ ρ ⇒ t')%term := (`λ subs (fun τ => Exts f ρ (σ:=τ)) t')%term;
+  subs f (λ ρ ⇒ t')%term := (`λ subs (Exts f ρ) t')%term;
   subs f (t₁ ⋅ t₂)%term  := (subs f t₁ ⋅ subs f t₂)%term.
 
 Equations sub_helper : forall {Γ τ}, Γ ⊢ τ -> forall τ', Has τ' (τ :: Γ) -> Γ ⊢ τ' :=
@@ -85,6 +85,7 @@ Inductive bred {Γ} : forall {τ}, Γ ⊢ τ -> Γ ⊢ τ -> Prop :=
     t₂ -->  t₂' ->
     (t₁ ⋅ t₂)%term -->  (t₁ ⋅ t₂')%term
 where "x '-->' y" := (bred x y) : type_scope.
+Derive Signature for bred.
 
 Print Assumptions bred.
 
@@ -108,7 +109,7 @@ Print Assumptions canonical_forms_abs.
 Local Hint Constructors bred : core.
 Local Hint Constructors value : core.
 
-Lemma Progress : forall {τ} (t : [] ⊢ τ), value t \/ exists t', t --> t'.
+Theorem Progress : forall {τ} (t : [] ⊢ τ), value t \/ exists t', t --> t'.
 Proof.
   intros τ t; depind t; eauto.
   - inv h.
@@ -118,3 +119,86 @@ Proof.
 Qed.
 
 Print Assumptions Progress.
+
+Lemma bred_exm : forall {Γ τ} (t : Γ ⊢ τ),
+    (exists t', t --> t') \/ forall t', ~ (t --> t').
+Proof.
+  intros Γ τ t; depind t; eauto.
+  - right; intros t' H; inv H.
+  - destruct IHt as [(t' & ih) | ih]; eauto.
+    right; intros t' H; depelim H.
+    apply ih in H; contradiction.
+  - destruct IHt1 as [(t1' & ih1) | ih1]; eauto.
+    destruct IHt2 as [(t2' & ih2) | ih2]; eauto.
+    depelim t1; eauto.
+    + right; intros t' H. depelim H.
+      * depelim H.
+      * apply ih2 in H; contradiction.
+    + right; intros t' H. depelim H.
+      * apply ih1 in H; contradiction.
+      * apply ih2 in H; contradiction.
+Qed.
+
+Notation "e '-->*' e'"
+  := (refl_trans_closure bred e e')
+       (at level 40, no associativity).
+
+(** Termination predicate. *)
+Definition halts {Γ τ} (t : Γ ⊢ τ) : Prop :=
+  exists t', t -->* t' /\ forall t'', ~ (t' -->  t'').
+
+(** Strongly Normalizing. *)
+Definition SN {Γ τ} : Γ ⊢ τ -> Prop := Acc (fun t' t => t -->  t').
+Equations Derive Signature for Acc.
+
+Section HaltsSN.
+  Local Hint Constructors refl_trans_closure : core.
+  
+  Lemma SN_halts : forall {Γ τ} (t : Γ ⊢ τ), SN t -> halts t.
+  Proof.
+    intros Γ τ t Hsn; unfold halts; depind Hsn.
+    destruct (bred_exm x) as [(t & Ht) | Ht]; eauto.
+    apply H0 in Ht as Ht_. destruct Ht_ as (t' & Ht' & Ht''); eauto.
+  Qed.
+End HaltsSN.
+
+Lemma subs_sub :
+  forall {Γ Δ} (σ : forall τ, Has τ Γ -> Δ ⊢ τ)
+    {τ τ'} (t₁ : τ :: Γ ⊢ τ') (t₂ : Γ ⊢ τ),
+    subs σ (t₁ [[t₂]])%term = (subs (Exts σ τ) t₁ [[subs σ t₂]])%term.
+Proof.
+  intros Γ Δ σ τ τ' t t'.
+  unfold "_ [[ _ ]]".
+  depind t.
+  - do 2 rewrite subs_equation_1.
+    depelim h.
+    + rewrite Exts_equation_1,subs_equation_1.
+      do 2 rewrite sub_helper_equation_1; reflexivity.
+    + rewrite Exts_equation_2,sub_helper_equation_2,
+        subs_equation_1. admit.
+  - do 4 rewrite subs_equation_2; f_equal.
+    admit (** Look to [SF/Dep.v]. *).
+  - do 4 rewrite subs_equation_3; f_equal; auto.
+Admitted.
+
+Lemma subs_bred : forall {Γ τ} (t t' : Γ ⊢ τ),
+    t -->  t' -> forall {Δ} (σ : forall τ, Has τ Γ -> Δ ⊢ τ),
+      subs σ t --> subs σ t'.
+Proof.
+  intros Γ τ t t' br; depind br; intros Δ σ.
+  - rewrite subs_equation_3,subs_equation_2.
+    assert (H:subs σ (t₁ [[t₂]])%term
+              = (subs (Exts σ τ) t₁ [[subs σ t₂]])%term)
+      by auto using subs_sub.
+    rewrite H; auto.
+  - do 2 rewrite subs_equation_2; auto.
+  - do 2 rewrite subs_equation_3; auto.
+  - do 2 rewrite subs_equation_3; auto.
+Qed.
+
+Lemma sub_bred : forall {Γ τ τ'} (bdy bdy' : τ :: Γ ⊢ τ'),
+    bdy --> bdy' -> forall (t : Γ ⊢ τ),
+    (bdy [[ t ]])%term --> (bdy' [[ t ]])%term.
+Proof.
+  eauto using subs_bred.
+Qed.
